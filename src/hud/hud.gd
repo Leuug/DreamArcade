b1a37e593cd53472622e5a612cbfcfaeb9f41db6
Misignu <gerson.creative.des@hotmail.com>
2020-05-02 18:48:36 -0300

    Fixed Hud: Weapon Display

--- src/hud/hud.gd
+++ src/hud/hud.gd
@@ -1,6 +1,7 @@
 extends CanvasLayer
 
-onready var score_display: Label = $Display/LeftContainer/Score/Value
+onready var score_display: Label = $Display/Top/Left/Score/Value
+onready var stamina_bar: ProgressBar = $Display/Top/Left/Stamina/Bar
 onready var exit_popup: Popup = $PopUp/Exit
 onready var animation_player: AnimationPlayer = $AnimationPlayer
 
@@ -20,8 +21,17 @@ func _unhandled_input(event: InputEvent) -> void:
 		get_tree().set_input_as_handled()
 
 
+# @signals
 func _on_Game_score_changed():
 	score_display.text = str(Game.score)
+
+
+func _on_Gerard_weapon_changed(weapon: String) -> void:
+	$Display/Top/Weapon/MarginContainer/Value.text = weapon
+
+
+func _on_Gerard_combo_changed(combo: int) -> void:
+	stamina_bar.value = stamina_bar.max_value - combo
 
 
 func _on_Exit_confirmed() -> void:
@@ -38,11 +48,3 @@ func _on_Exit_popup_hide() -> void:
 	
 	get_tree().paused = false
 	animation_player.play_backwards("blur_in")
-
-
-func _on_Gerard_weapon_changed(weapon: String) -> void:
-	$Display/Weapon/Value.text = weapon
-
-
-func _on_Gerard_combo_changed(combo: int) -> void:
-	$Display/LeftContainer/Stamina/Bar.value = $Display/LeftContainer/Stamina/Bar.max_value - combo
